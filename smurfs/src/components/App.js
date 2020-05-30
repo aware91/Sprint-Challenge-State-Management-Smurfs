6ddede34b18a6f1d8953475e46612cc14da05951
Anthony Ware <aware91coding@gmail.com>
2020-05-29 22:23:54 -0500

    Think I have finished MVP, will go over ReadMe to double check before submit

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import "./App.css";
 import axios from 'axios';
 import SmurfsData from './SmurfsData';
 import SmurfsForm from './SmurfsForm';
+
+export const SmurfsContext = createContext();
+console.log('aw: App.js: SmurfsContext: ', SmurfsContext)
 
 function App()  {
   const [smurf, setSmurf] = useState([]);
@@ -21,12 +24,14 @@ function App()  {
 
   return (
     <div className="App">
-      <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
-      <SmurfsForm />
-      <SmurfsData smurf={smurf} />
+      <SmurfsContext.Provider value={smurf}>
+        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <div>Welcome to your state management version of Smurfs!</div>
+        <div>Start inside of your `src/index.js` file!</div>
+        <div>Have fun!</div>
+        <SmurfsForm />
+        <SmurfsData smurf={smurf} />
+      </SmurfsContext.Provider>
     </div>
   );
 }
--- smurfs/src/components/SmurfsData.js
+++ smurfs/src/components/SmurfsData.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SmurfsContext } from './App';
 
 const SmurfsData = props => {
-    console.log('aw: SmurfsData.js: props.smurf: ', props.smurf)
+    const smurf = useContext(SmurfsContext);
+
+    console.log('aw: SmurfsData.js: props.smurf: ', smurf)
     return (
         <div>
             <h1>Smurfs</h1>
             <div className='smurfCard'>
-                {props.smurf.map((smurfs)=>(
+                {smurf.map((smurfs)=>(
                     <div key={smurfs.id} className='smurfsCard'>
                         <p>Name: {smurfs.name}</p>
                         <p>Age: {smurfs.age}</p>
