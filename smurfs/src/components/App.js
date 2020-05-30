0fa01416ef2f0caa19418264daf05303ac9e47cc
Anthony Ware <aware91coding@gmail.com>
2020-05-29 20:47:36 -0500

    got the smurf data to show in my state

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
+import SmurfsData from './SmurfsData';
 
 function App()  {
   const [smurf, setSmurf] = useState([]);
@@ -9,16 +10,13 @@ function App()  {
     axios
         .get('http://localhost:3333/smurfs')
         .then(res => {
-            console.log('aw: App.js: axios: ', res)
-        })
-        .then(data=>{
-          console.log('aw: App.js: data: ', data)
-          setSmurf(data)
+            setSmurf(res.data)
+            console.log('aw: App.js: axios: ', res.data)
         })
         .catch(err => {
             console.log(err)
         })
-    })
+    }, [])
 
   return (
     <div className="App">
@@ -26,6 +24,7 @@ function App()  {
       <div>Welcome to your state management version of Smurfs!</div>
       <div>Start inside of your `src/index.js` file!</div>
       <div>Have fun!</div>
+      <SmurfsData />
     </div>
   );
 }
--- smurfs/src/components/SmurfsData.js
+++ smurfs/src/components/SmurfsData.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function SmurfData() {
-
+const SmurfsData = props => {
+    return (
+        <div>
+            <p>{props.smurf.name}</p>
+            <p>{props.smurf.age}</p>
+            <p>{props.smurf.height}</p>
+        </div>
+    )
 }
 
-export default SmurfData;
+export default SmurfsData;
